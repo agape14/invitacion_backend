9063a5e54100cd61bae64f56160ff69f4a20a066
agape14 <adelacruzcarlos@gmail.com>
2026-01-15 17:39:55 -0500

    mejora los colores de la invitacion

---
+++ app/Http/Controllers/Api/SettingController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    public function index()
+    {
+        $settings = Setting::all()->pluck('value', 'key');
+        return response()->json($settings);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'lugar' => 'nullable|string',
+            'direccion' => 'nullable|string',
+            'referencia' => 'nullable|string',
+            'mapLink' => 'nullable|string',
+        ]);
+
+        foreach ($data as $key => $value) {
+            Setting::updateOrCreate(
+                ['key' => $key],
+                ['value' => $value]
+            );
+        }
+
+        return response()->json(['success' => true, 'message' => 'Settings updated successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,7 @@ use App\Http\Controllers\Api\InvitadoController;
 
 Route::prefix('v1')->group(function () {
     Route::post('/invitados', [InvitadoController::class, 'store']);
+    Route::get('/settings', [App\Http\Controllers\Api\SettingController::class, 'index']);
+    Route::post('/settings', [App\Http\Controllers\Api\SettingController::class, 'store']);
 });
 
